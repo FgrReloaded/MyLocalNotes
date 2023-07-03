5e4fc0ff8ba6e1e59ce003b61467285e2c4b5aca
Nitish <nk.nitish.nk500@gmail.com>
2023-07-03 17:44:05 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-// const mongoURI = "mongodb+srv://fgrreloaded:f@g#r$14@nodedb.nd4t9.mongodb.net/inotebook?retryWrites=true&w=majority";
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI = process.env.MONGOURI;
+// const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 
 const connectToMongo =  ()=>{
