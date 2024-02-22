f125c4335a1efe835d7193cdf9dab34ecde3e129
Nitish <nk.nitish.nk500@gmail.com>
2024-02-22 23:11:24 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,46 +1,17 @@
+// Connect to MongoDB
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const MONGODB_URL = process.env.DB_CONNECT
-if (!MONGODB_URL) {
-    throw new Error(
-        "Please define the MONGODB_URI environment variable inside .env.local"
-    )
-}
 
-
-let cached = global.mongoose;
-
-if (!cached) {
-    cached = global.mongoose = {con: null, promise: null}
-}
-
-const dbConnect = async () => {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-
-// If a connection does not exist, we check if a promise is already in progress. If a promise is already in progress, we wait for it to resolve to get the connection
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands : false
-        };
-
-        cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
-            console.log("Database Connected")
-            return mongoose
-        })
-    }
-
+const dbConnect = () => {
     try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        throw e;
+        mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDB is connected");
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
     }
-
-    return cached.conn;
 }
 
 module.exports = dbConnect;
